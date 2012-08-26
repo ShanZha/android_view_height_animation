3bb58d85f11ee5f3a242eda8b413b90a38c8a4d2
Vitaliy Khudenko <vitaliy.khudenko@gmail.com>
2012-08-26 16:44:07 +0300

    initial implementation

---
+++ src/vit/android/test/height/animation/ChangeYSizeAnimation.java
@@ -0,0 +1,176 @@
+package vit.android.test.height.animation;
+
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+/**
+ * This class allows to change vertical size (height + margins) of the view.
+ */
+public class ChangeYSizeAnimation extends Animation {
+
+    /**
+     * Describes target height in pixels for the view (this doesn't touch view
+     * margins).
+     */
+    public static class HeightSpec {
+        
+        public final int target;
+        
+        public HeightSpec(int target) {
+            this.target = target;
+        }
+        
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName() + " { target: " + target + " }";
+        }
+    }
+    
+    /**
+     * Describes target top/bottom margins in pixels for the view.
+     */
+    public static class MarginSpec {
+        
+        public final int targetTop;
+        public final int targetBottom;
+        
+        public MarginSpec(int targetTop, int targetBottom) {
+            this.targetTop    = targetTop;
+            this.targetBottom = targetBottom;
+        }
+        
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName() 
+                    + " { targetTop: " + targetTop
+                    + ", targetBottom: " + targetBottom + " }";
+        }
+    }
+    
+    private static final String TAG = ChangeYSizeAnimation.class.getSimpleName();
+    
+    private static final boolean DEBUG = false;
+    
+    private View view;
+    private ViewGroup.MarginLayoutParams layoutParams;
+    private int initialHeight;
+    private int deltaHeight;
+    
+    private int initialMarginTop, initialMarginBottom;
+    private int deltaMarginTop, deltaMarginBottom; 
+    
+    private boolean process        = false;
+    private boolean processHeight  = false;
+    private boolean processMargins = false;
+    
+    private HeightSpec heightSpec;
+    private MarginSpec marginSpec;
+
+    /**
+     * @param view
+     *            - view to be processed, should NOT be null.
+     * @param heightSpec
+     *            - HeightSpec instance to describe target height in pixels for
+     *            the view (this doesn't touch view margins). Can be null.
+     * @param marginSpec
+     *            - MarginSpec instance to describe target top/bottom margins in
+     *            pixels for the view. Can be null.
+     */
+    public ChangeYSizeAnimation(View view, HeightSpec heightSpec, MarginSpec marginSpec) {
+        processHeight  = heightSpec != null;
+        processMargins = marginSpec != null;
+        
+        if (processHeight || processMargins) {
+            process = true;
+            
+            this.view = view;
+            
+            layoutParams = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+            
+            if (processHeight) {
+                
+                this.heightSpec = heightSpec;
+                
+                if (layoutParams.height < 0) {
+                    // this is possible because height can be one of these 2 constants: 
+                    // ViewGroup.LayoutParams.MATCH_PARENT = -1;
+                    // ViewGroup.LayoutParams.WRAP_CONTENT = -2;
+                    
+                    // note this assumes that the view has been already measured
+                    initialHeight = view.getHeight();
+                } else {
+                    initialHeight = layoutParams.height;
+                }
+                
+                deltaHeight = heightSpec.target - initialHeight;
+            }
+            
+            
+            if (processMargins) {
+                this.marginSpec = marginSpec;
+                
+                initialMarginTop    = layoutParams.topMargin;
+                initialMarginBottom = layoutParams.bottomMargin;
+                
+                deltaMarginTop    = marginSpec.targetTop    - initialMarginTop;
+                deltaMarginBottom = marginSpec.targetBottom - initialMarginBottom;
+            }
+
+        }
+        
+        if (DEBUG) {
+            log("in constructor");
+            log("initial height = " + initialHeight + ", delta = " + deltaHeight);
+            log("initial topMargin = " + layoutParams.topMargin + ", bottomMargin = " + layoutParams.bottomMargin);
+            log(heightSpec.toString());
+            log(marginSpec.toString());
+        }
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        if (!process) return;
+        
+        ViewGroup.MarginLayoutParams lp = layoutParams;
+        
+        // let's handle corner cases separately to avoid casting/rounding issues
+        if (interpolatedTime == 0.0f) { /* start of the animation */
+            // just do nothing in this case
+        } else if (interpolatedTime == 1.0f) { /* end of the animation */
+            if (processHeight) {
+                lp.height = heightSpec.target;
+            }
+            if (processMargins) {
+                lp.topMargin    = marginSpec.targetTop;
+                lp.bottomMargin = marginSpec.targetBottom;
+            }
+        } else {
+            if (processHeight) {
+                lp.height = initialHeight + (int) (deltaHeight * interpolatedTime);
+            }
+            if (processMargins) {
+                lp.topMargin    = initialMarginTop    + (int) (deltaMarginTop    * interpolatedTime);
+                lp.bottomMargin = initialMarginBottom + (int) (deltaMarginBottom * interpolatedTime);
+            }
+        }
+        
+        if (DEBUG) {
+            log("applyTransformation: new height: " + lp.height + "; new margins: "
+                    + lp.topMargin + ", " + lp.bottomMargin);
+        }
+        
+        view.requestLayout();
+    }
+
+    @Override
+    public boolean willChangeBounds() {
+        return true;
+    }
+    
+    private static void log(String string) {
+        Log.d(TAG, string);
+    }
+}
